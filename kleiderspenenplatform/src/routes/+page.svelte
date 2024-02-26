bc6c3b7a1a4839e068317c6bd3a813834f94f734
dasete <ete1@hotmail.de>
2024-02-26 20:39:50 +0100

    Rechtschreibprüfung erfolgt

--- kleiderspenenplatform/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<div class="flex flex-col mx-auto text-center">
-<h1>Wilkommen beim Apparel Relief</h1>
-
-<p>Wenn Sie Kleidung spenden wollen, gehen Sie oben rechts auf den Reiter <span class="font-bold text-emerald-300">Kleiderspende</span>  und füllen Sie das Formular aus. Sie können selbst darüber entscheiden,  in welche Krisengebiet die Spende geschickt werden soll. Auch holen wir die Kleidung gerne bei Ihrer Wunschadresse ab, wenn Sie bei uns in der nähe sind. Vielen Dank für Ihr Mitmachen!</p>
-</div>
-
-
-  
